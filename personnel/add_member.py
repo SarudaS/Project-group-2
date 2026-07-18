abab7d382fedd2ec9f2d460d93005aef8ed5372d
saruda <saruda2007@gmail.com>
2026-07-18 15:33:25 +0700

    personnel_add_remove

--- personnel/add_member.py
+++ personnel/add_member.py
@@ -5,14 +5,70 @@
 from data import family_members
 
 def add_member(name, age, power, money):
-#   - คำนวณ role: power >= 8 -> "Hitman" | money >= 1000000 -> "Sponsor" | นอกนั้น -> "Slave"
-#   - สร้าง dict สมาชิกใหม่ (key: name, age, role, power, money, equipment เริ่มต้น "ไม่มี")
-#   - เพิ่มเข้า family_members แล้ว return dict นั้น
-    # TODO: เขียนโค้ดตรงนี้
-    pass
+ 
+    name = input("ชื่อ: ")
+    age = int(input("อายุ: "))
+    role = "Slave"
+    power = int(input("ความโหด (1-10): "))
+    money = float(input("เงินส่วย: "))
+    equipment = input("อุปกรณ์: ")
+
+    new_member = {
+        "name" : name,
+        "age" : age,
+        "role" : role,
+        "power" : 0,
+        "money" : 0,
+        "equipment" : equipment,
+    }
+
+    if power >= 8 :
+        new_member["role"] = "Hitman"
+        new_member["power"] = power
+        
+    elif money >= 1000000 :
+        new_member["role"] = "Sponsor"
+        new_member["money"] = money
+
+    if equipment == "":
+        new_member["equipment"] = "ไม่มี"
+
+    family_members.append(new_member)
+    
+    return(add_member)   # ต้องเห็น Vito ต่อท้ายลิสต์ และ role เป็น Hitman
+
 
 
 # ทดสอบเฉพาะไฟล์ตัวเอง: พิมพ์  python -m personnel.add_member
 if __name__ == "__main__":
-    add_member("Vito", 20, 9, 500)
+    
+    name = input("ชื่อ: ")
+    age = int(input("อายุ: "))
+    role = "Slave"
+    power = int(input("ความโหด (1-10): "))
+    money = float(input("เงินส่วย: "))
+    equipment = input("อุปกรณ์: ")
+
+    new_member = {
+        "name" : name,
+        "age" : age,
+        "role" : role,
+        "power" : 0,
+        "money" : 0,
+        "equipment" : equipment,
+    }
+
+    if power >= 8 :
+        new_member["role"] = "Hitman"
+        new_member["power"] = power
+        
+    elif money >= 1000000 :
+        new_member["role"] = "Sponsor"
+        new_member["money"] = money
+
+    if equipment == "":
+        new_member["equipment"] = "ไม่มี"
+
+    family_members.append(new_member)
+    
     print(family_members)   # ต้องเห็น Vito ต่อท้ายลิสต์ และ role เป็น Hitman
